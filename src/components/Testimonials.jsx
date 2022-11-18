97a6bb907dc4c8bb4b271947cd2002977218fb5c
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2022-11-18 00:36:08 -0600

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,12 +8,11 @@ const Testimonials = () => (
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
       <h2 className={styles.heading2}>
-        What People are <br className="sm:block hidden" /> saying about us
+        Who I Am And<br className="sm:block hidden" /> My Expertise
       </h2>
       <div className="w-full md:mt-0 mt-6">
         <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-          Everything you need to accept card payments and grow your business
-          anywhere on the planet.
+          I have 3 years of experience running successful online businesses that earn income through affilate marketing and ads.
         </p>
       </div>
     </div>
