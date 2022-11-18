d271cacd84ea1f279a46ae30915dcc2432119033
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2022-11-18 00:30:52 -0600

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -19,10 +19,11 @@ import discount from "./Discount.svg";
 import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
-import twitter from "./twitter.svg";
+import twitter from "./twitter.png";
 import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
+import gitbook from "./gitbook.png"
 
 export {
   airbnb,
@@ -50,4 +51,5 @@ export {
   people01,
   people02,
   people03,
+  gitbook,
 };
